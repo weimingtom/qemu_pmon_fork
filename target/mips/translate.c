eeb2d11ead8125552efbe4389f12c28c02e50887
QiaoChong <qiaochong@loongson.cn>
2016-05-04 09:00:04 +0800

    add ls2h gssq, gslq instruction support.
    Signed-off-by: qiaochong <qiaochong@loongson.cn>

--- target/mips/translate.c
+++ target/mips/translate.c
@@ -20047,6 +20047,34 @@ static void decode_opc(CPUMIPSState *env, DisasContext *ctx)
             /* COP2: Not implemented. */
             generate_exception_err(ctx, EXCP_CpU, 2);
         }
+    /* COP2.  */
+    case OPC_LWC2:
+	{
+	int offset = ((ctx->opcode >> 6)&0x1ff)<<4;
+	int rq =  ctx->opcode & 0x1f;
+    	int rs = (ctx->opcode >> 21) & 0x1f;
+    	int rt = (ctx->opcode >> 16) & 0x1f;
+        gen_ld(ctx, OPC_LD, rt, rs, offset);
+        gen_ld(ctx, OPC_LD, rq, rs, offset+8);
+	}
+
+	break;
+    case OPC_SWC2:
+	{
+	int offset = ((ctx->opcode >> 6)&0x1ff)<<4;
+	int rq =  ctx->opcode & 0x1f;
+    	int rs = (ctx->opcode >> 21) & 0x1f;
+    	int rt = (ctx->opcode >> 16) & 0x1f;
+        gen_st(ctx, OPC_SD, rt, rs, offset);
+        gen_st(ctx, OPC_SD, rq, rs, offset+8);
+	}
+
+	break;
+
+    case OPC_LDC2:
+    case OPC_SDC2:
+        /* COP2: Not implemented. */
+        generate_exception_err(ctx, EXCP_CpU, 2);
         break;
     case OPC_CP2:
         check_insn(ctx, INSN_LOONGSON2F);
