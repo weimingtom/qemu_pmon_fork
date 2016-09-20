9b3163612f217cf8059ab5dd050d7045dd3aa4bb
QiaoChong <qiaochong@loongson.cn>
2016-09-20 22:11:44 +0800

    fix ls1a gpio irq.
    Signed-off-by: QiaoChong <qiaochong@loongson.cn>

--- hw/mips/mips_ls1a.c
+++ hw/mips/mips_ls1a.c
@@ -495,7 +495,7 @@ static void mips_ls1a_init (MachineState *args)
 			qdev_init_nofail(dev1);
 		}
 		else dev1 = ssi_create_slave(bus, "ssi-sd");
-		cs_line = qdev_get_gpio_in(dev1, 0);
+		cs_line = qdev_get_gpio_in_named(dev1, "ssi-gpio-cs",  0);
 		sysbus_connect_irq(SYS_BUS_DEVICE(dev), 1 , cs_line);
 	}
 
